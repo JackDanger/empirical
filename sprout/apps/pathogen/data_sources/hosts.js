c1b48ca996e108ffbdd6d369e6cbc2681dd6a869
Jack Danger Canty <gitcommit@6brand.com>
2011-02-06 12:23:04 -0800

    adding data source

---
+++ sprout/apps/pathogen/data_sources/hosts.js
@@ -0,0 +1,73 @@
+// ==========================================================================
+// Project:   Pathogen.Hosts
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Pathogen */
+
+/** @class
+
+  (Document Your Data Source Here)
+
+  @extends SC.DataSource
+*/
+Pathogen.HostDataSource = SC.DataSource.extend(
+/** @scope Pathogen.Hosts.prototype */ {
+
+  // ..........................................................
+  // QUERY SUPPORT
+  // 
+
+  fetch: function(store, query) {
+    SC.Request.getUrl('http://localhost:8098/riak?buckets=true').json()
+      .notify(this, this.didFetchHosts, store, query)
+      .send()
+    // TODO: Add handlers to fetch data for specific queries.  
+    // call store.dataSourceDidFetchQuery(query) when done.
+
+    return NO ; // return YES if you handled the query
+  },
+
+  didFetchHosts: function(response, store, query){
+    if (SC.ok(response))
+      store.loadRecords(Pathogen.Host, resonse.get('body'))
+    else
+      store.dataSourceDidErrorQuery(query, response)
+  }
+
+  // ..........................................................
+  // RECORD SUPPORT
+  // 
+  
+  retrieveRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to retrieve an individual record's contents
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  createRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit new records to the data source.
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  updateRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit modified record to the data source
+    // call store.dataSourceDidComplete(storeKey) when done.
+
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  destroyRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to destroy records on the data source.
+    // call store.dataSourceDidDestroy(storeKey) when done
+    
+    return NO ; // return YES if you handled the storeKey
+  }
+  
+}) ;
