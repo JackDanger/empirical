cd098b7ded70ca7faf70980e4f4b3e99b9dcc4ba
Jack Danger Canty <gitcommit@6brand.com>
2011-02-12 13:55:59 -0800

    organizing a boot script

--- app.js
+++ app.js
@@ -1,53 +1,8 @@
 
-var sys  = require('sys'),
-    util = require('util'),
-    http = require('http'),
-    url  = require('url')
-
-var riak = require('./lib/model')
-
-http.createServer(function(request, response){
-
-  var uri = url.parse(request.url, true)
-
-  sys.puts(request.url)
-
-  if( '/' == uri.pathname ){
-    sys.puts('found!')
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.end('<html>H1 there</html>')
-
-
-  } else if ( '/hosts' == uri.pathname ){
-
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    riak.getHosts(function(hosts){
-      response.end(JSON.stringify(hosts))
-    })
-
-  } else if ( '/sessions' == uri.pathname ) {
-
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    riak.getSessions(uri.query['host'],
-                     function(sessions){
-                       response.end(JSON.stringify(sessions))
-                     })
-
-  } else if ( '/paths' == uri.pathname ) {
-
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    riak.getPaths(uri.query['host'],
-                  uri.query['session'],
-                  function(sessions){
-                    response.end(JSON.stringify(sessions))
-                  })
-
-  } else {
-
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.end('Error: could not find "'+uri.pathname+'"')
-    
-  }
-
-}).listen(9000)
+// require('./lib/web')
+require('./lib/model')
+require('fs').readdir('models', function(_, files){
+  for (i in files)
+    require('./models/'+files[i])
+})
 
