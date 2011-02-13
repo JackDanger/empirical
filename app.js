29b851056c890685458aefa3db76c9602e6d5317
Jack Danger Canty <gitcommit@6brand.com>
2011-02-12 17:00:01 -0800

    using my own arg-parser to start Web

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
-// require('./lib/web')
+require('./lib/web')
 require('./lib/model')
 require('fs').readdir('models', function(_, files){
   for (i in files)
     require('./models/'+files[i])
 })
 
+for (i in process.argv){
+  if ('--port' == process.argv[i]){
+    Web.port = process.argv[parseInt(i)+1]
+    require('sys').puts("Starting Empirical web app on port "+Web.port)
+    Web.listen(Web.port)
+  }
+}
